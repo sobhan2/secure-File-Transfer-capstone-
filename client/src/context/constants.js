058800e86680094ffea4a5c51688f349726bb440
sobhan2 <sobhanhero2001@gmail.com>
2024-05-14 16:26:59 +0530

    new start

--- client/src/context/constants.js
+++
@@ -1,4 +0,0 @@
-export const reducerCases = {
-     SET_USER_INFO: "SET_USER_INFO",
-     SET_NEW_USER: "SET_NEW_USER",
-};
--- server/routes/AuthRoutes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import { checkUser } from "../controllers/AuthController.js";
-
-
-const router = Router()
-
-router.post("/check-user", checkUser);
-
-export default router;
